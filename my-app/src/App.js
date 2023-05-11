6a62a275e60c6fd53d6bde01e0bbf766e39e4f28
Lovro Vcelik <lvcelik8@gmail.com>
2023-05-11 18:07:45 +0200

    Created header component, added Sidebar component in App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 import Chat from "./components/Chat";
+import Header from "./components/Header";
 import Login from "./components/Login";
+import Sidebar from "./components/Sidebar";
 
 class App extends React.Component{
   constructor(props){
@@ -13,13 +15,21 @@ class App extends React.Component{
   handleLogin = (username) =>{
     this.setState({username})
   }
-
+  toggleSidebar = () =>{
+    this.sidebar.ToggleSidebar();
+  }
 
   render(){
-    return <Routes>
+    return (
+      <div> 
+      <Header toggleSidebar={this.toggleSidebar} username={this.state.username}/>
+      <Sidebar ref={(reference) => this.sidebar = reference}/>
+      <Routes>
       <Route path="/" element={ this.state.username ?  <Chat username={this.state.username}/> : <Navigate to="/login" />} />
       <Route path="/login" element={<Login onLogin={this.handleLogin}/>} />
     </Routes>
+    </div>
+    );
   }
 }
 
